89a3d60e3deb30fa1318d6664db4fe8626c27107
Ornella <ornelladelvicario@gmail.com>
2025-04-23 09:40:53 +0200

    Added Document, Electronic and test class

--- src/SmartBox.java
+++ src/SmartBox.java
@@ -1,2 +1,13 @@
-public class SmartBox {
+public class SmartBox<T> {
+    private T item;
+
+    // Method to add an item
+    public void addItem(T item) {
+        this.item = item;
+    }
+
+    // Method to obtain an item
+    public T getItem() {
+        return item;
+    }
 }
